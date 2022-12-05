5a4fa8a2dd9f93da4b4f38cae892ca802257591b
nicolas <114409333+nicolas-acourt@users.noreply.github.com>
2022-12-05 11:14:47 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="CSS/style.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+    <title>index</title>
+</head>
+<body>
+        <h1 class="fond">page d'accueil</h1>
+<section>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+            <li class="page-item"><a class="page-link" href="#" >connexion</a>
+            
+            <li class="page-item"><a class="page-link" href="#">panier</a></li>
+        </ul>
+    </nav>
+</section>
+</body>
+</html>
